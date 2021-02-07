5c7e6ea87349de6b0bdd0bfac0d828da71dd8a1f
Casiopa <anablancodelgado@gmail.com>
2021-02-07 23:18:07 +0100

    Commit

--- relationships.py
+++ relationships.py
@@ -1,17 +1,53 @@
 import json
-#import sqlite3
+import sqlite3
 
-#fname = 'relationships.json'
-#str_data = open(fname).read()
-#json_data = json.loads(str_data)
+conn = sqlite3.connect('areas.sqlite')
+cur = conn.cursor()
 
 
+### Create and fill table for health areas (zbs) related with its fathers
 fname = 'mun_distr_childs.json'
 str_mun_distr_childs = open(fname).read()
 json_mun_distr_childs = json.loads(str_mun_distr_childs)
 
-count = 0;
-for zbs in json_mun_distr_childs :
-    print(zbs)
-    count += 1
-    print(count)
+cur.execute('DROP TABLE IF EXISTS ZBS_fathers')
+cur.execute('''CREATE TABLE ZBS_fathers (zbs_id INTEGER UNIQUE, mun_distr_id INTEGER)''')
+
+for mun_distr in json_mun_distr_childs :
+    cur.execute('SELECT id FROM MunDistr WHERE name = ? LIMIT 1', (mun_distr,))
+    mun_distr_id = cur.fetchone()[0]
+
+    for zbs in json_mun_distr_childs[mun_distr] :
+        cur.execute('SELECT id FROM ZBS WHERE name = ? LIMIT 1', (zbs,))
+        zbs_id = cur.fetchone()[0]
+        cur.execute('''INSERT OR IGNORE INTO ZBS_fathers (zbs_id, mun_distr_id)
+                    VALUES (?,?)''', (zbs_id, mun_distr_id))
+        conn.commit()
+
+
+### Create and fill table for political areas (Municipios y Distritos) related with its fathers
+fname = 'zbs_childs.json'
+str_zbs_childs = open(fname).read()
+json_zbs_childs = json.loads(str_zbs_childs)
+
+cur.execute('DROP TABLE IF EXISTS MunDistr_fathers')
+cur.execute('''CREATE TABLE MunDistr_fathers (mun_distr_id INTEGER UNIQUE, zbs_id INTEGER)''')
+
+for zbs in json_zbs_childs :
+    cur.execute('SELECT id FROM ZBS WHERE name = ? LIMIT 1', (zbs,))
+    zbs_id = cur.fetchone()[0]
+
+    for mun_distr in json_zbs_childs[zbs] :
+        cur.execute('SELECT id FROM MunDistr WHERE name = ? LIMIT 1', (mun_distr,))
+        print(mun_distr, zbs)
+        print(cur.fetchone(), zbs_id)
+        temp = cur.fetchone()
+        print(type(temp))
+        mun_distr_id = temp[0]
+        #print(cur.fetchone()[0])
+        #print(type(cur.fetchone()[0]))
+        #cur.execute('''INSERT OR IGNORE INTO MunDistr_fathers (mun_distr_id, zbs_id)
+        #            VALUES (?,?)''', (mun_distr_id, zbs_id))
+        #conn.commit()
+
+cur.close()
