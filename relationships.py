59c1ea09408b4a4e6ac2828c1e063ce3535f949d
Casiopa <anablancodelgado@gmail.com>
2021-02-07 19:52:13 +0100

    Actualizar relationships.py Añadir mun_distr_childs.json

--- relationships.py
+++ relationships.py
@@ -1,2 +1,17 @@
 import json
-import sqlite3
+#import sqlite3
+
+#fname = 'relationships.json'
+#str_data = open(fname).read()
+#json_data = json.loads(str_data)
+
+
+fname = 'mun_distr_childs.json'
+str_mun_distr_childs = open(fname).read()
+json_mun_distr_childs = json.loads(str_mun_distr_childs)
+
+count = 0;
+for zbs in json_mun_distr_childs :
+    print(zbs)
+    count += 1
+    print(count)
